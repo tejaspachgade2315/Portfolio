fbbf8edb02cb1746384baa08fd02fa1fa45edf03
tejaspachgade2315 <149186886+aeonstechdevops@users.noreply.github.com>
2025-02-11 13:52:44 +0530

    PWA 2

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ self.addEventListener("install", (event) => {
         })
     );
 });
-
 self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
